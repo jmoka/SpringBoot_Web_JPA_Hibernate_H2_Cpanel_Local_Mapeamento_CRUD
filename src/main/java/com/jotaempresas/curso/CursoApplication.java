83047d2393c5c660b2f16df5a034b4d87ededfed
teusiaqe <jmokatavares@gmail.com>
2025-08-04 18:35:47 -0300

    implamentação do H2, mapeamento da entydade user, criação e configuração do application.properties

--- src/main/java/com/jotaempresas/curso/CursoApplication.java
+++ src/main/java/com/jotaempresas/curso/CursoApplication.java
@@ -2,8 +2,10 @@ package com.jotaempresas.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.jotaempresas.curso.entity")
 public class CursoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jotaempresas/curso/entity/User.java
+++ src/main/java/com/jotaempresas/curso/entity/User.java
@@ -3,17 +3,32 @@ package com.jotaempresas.curso.entity;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
+@Entity
+@Table(name="tb_user") // usar sempre tb_ para não dar conflito
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	
 	// 1) Atributops Básicos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID_USER")
 	private Long id;
+	@Column(name="NOME_USER")
 	private String name;
+	@Column(name="EMAIL_USER")
 	private String email;
+	@Column(name="PHONE_USER")
 	private String phone;
+	@Column(name="PASSWORD_USER")
 	private String password;
 	
 	// Associações
